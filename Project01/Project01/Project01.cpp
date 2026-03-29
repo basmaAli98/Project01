2c64857907cc8a611cce02ac5711e8dc68d460bf
basmaAli98 <basmaali0125@gmail.com>
2026-03-29 04:47:52 +0200

    Solution 1

--- Project01/Project01/Project01.cpp
+++ Project01/Project01/Project01.cpp
@@ -1,11 +1,10 @@
-// Project01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int i = 0; i <= 5; i++) {
+        cout << i << " ";
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
